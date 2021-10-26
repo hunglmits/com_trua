40289bbe3891775505983ebdfcdaf972c4bcc6db
Leo <>
2021-10-26 22:47:55 +0700

    open link by browser

--- src/screens/ZweiWebview/index.js
+++ src/screens/ZweiWebview/index.js
@@ -1,15 +1,22 @@
 // import analytics from '@react-native-firebase/analytics'
 import React, { useState, useCallback, useEffect, useRef } from "react";
-import { View, StyleSheet, Platform, ActivityIndicator } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Platform,
+  ActivityIndicator,
+  Linking,
+} from "react-native";
 
 import { WebView } from "react-native-webview";
-import { Constants } from "../../utils";
 import { useNotificationHandler } from "../../hooks";
 
 import messaging from "@react-native-firebase/messaging";
 
-const BASE_URL =
-  "https://zwei-test:MsVfM7aVBf@stg5-4.zwei-test.com/members/sign_in";
+const ORIGIN_URL = "stg4.zwei-test.com";
+const APP_PARAM = "?flag_app=true";
+const BASE_URL = `https://zwei-test:MsVfM7aVBf@${ORIGIN_URL}/members/sign_in${APP_PARAM}`;
 
 const ZweiWebview = () => {
   const { notificationHandler, notiData } = useNotificationHandler();
@@ -18,7 +25,6 @@ const ZweiWebview = () => {
   const [deviceToken, setDeviceToken] = useState("");
   const [deviceType, setDeviceType] = useState("");
   const [url, setUrl] = useState(BASE_URL);
-
   const webviewRef = useRef();
 
   useEffect(() => {
@@ -26,7 +32,11 @@ const ZweiWebview = () => {
   }, []);
 
   useEffect(() => {
-    setUrl(notiData?.url || BASE_URL);
+    if (notiData?.url) {
+      setUrl(`${notiData?.url}${APP_PARAM}`);
+      return;
+    }
+    setUrl(BASE_URL);
   }, [notiData]);
 
   const initNotification = useCallback(() => {
@@ -64,7 +74,17 @@ const ZweiWebview = () => {
         showsVerticalScrollIndicator={false}
         javaScriptEnabled={true}
         javaScriptEnabledAndroid={true}
-        onMessage={(event) => {}}
+        onShouldStartLoadWithRequest={(event) => {
+          if (!event.url.includes(ORIGIN_URL)) {
+            Linking.openURL(event.url);
+            return false;
+          }
+          if (!event.url.includes(APP_PARAM)) {
+            setUrl(`${event.url}${APP_PARAM}`);
+            return false;
+          }
+          return true;
+        }}
         injectedJavaScript={js}
         startInLoadingState={true}
         renderLoading={renderLoadingIndicatorView}
@@ -73,16 +93,18 @@ const ZweiWebview = () => {
     );
   };
 
-  return <View style={styles.container}>{renderWebview()}</View>;
+  return (
+    <SafeAreaView style={styles.container}>{renderWebview()}</SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
   },
   webview: {
-    marginTop: Constants.layout.navPadding,
-    marginBottom: Constants.layout.navPadding / 2,
+    flex: 1,
   },
   loadingWrapper: {
     flex: 1,

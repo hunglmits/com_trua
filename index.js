d0247ea7955dbc094ffcb503b6ee481a3910d6f1
sybvits <sybv@h-basis.co.jp>
2022-05-15 19:15:59 +0700

    update delete code push and appcenter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from "react-native";
-import AppWrappedInCodePush from "./AppWrappedInCodePush";
+import App from "./App";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => AppWrappedInCodePush);
+AppRegistry.registerComponent(appName, () => App);

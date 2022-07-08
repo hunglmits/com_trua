f4ea4a78b8b6b33be6bfea98634e22428818db20
hunglm <hunglm@its-global.vn>
2022-07-08 19:23:36 +0700

    STG 5-3

--- src/screens/ZweiWebview/index.js
+++ src/screens/ZweiWebview/index.js
@@ -14,26 +14,26 @@ import messaging from "@react-native-firebase/messaging";
 // TODO: DEV
 // const ORIGIN_URL = "dev.zwei-test.com";
 // TODO: STG5-3
-// const ORIGIN_URL = "stg5-3.zwei-test.com";
-// TODO: Localhost
-const ORIGIN_URL = "192.168.1.127:3001";
-const ORIGIN_URL_SIGN_IN = `http://${ORIGIN_URL}/members/sign_in`;
-const ORIGIN_URL_SIGN_OUT = `http://${ORIGIN_URL}/members/sign_out`;
-const ORIGIN_URL_NEWS = `http://${ORIGIN_URL}/news`;
-const ORIGIN_URL_PASSWORD_NEWS = `http://${ORIGIN_URL}/members/password/new`;
-const APP_PARAM = "flag_app=true";
-const BASE_URL = `http://${ORIGIN_URL}`;
-const PARAM_URL = `${BASE_URL}?${APP_PARAM}`;
+const ORIGIN_URL = "stg5-3.zwei-test.com";
 // TODO: Product
 // const ORIGIN_URL = "app.zwei.ne.jp";
-
-// const ORIGIN_URL_SIGN_IN = `https://${ORIGIN_URL}/members/sign_in`;
-// const ORIGIN_URL_SIGN_OUT = `https://${ORIGIN_URL}/members/sign_out`;
-// const ORIGIN_URL_NEWS = `https://${ORIGIN_URL}/news`;
-// const ORIGIN_URL_PASSWORD_NEWS = `https://${ORIGIN_URL}/members/password/new`;
+// TODO: Localhost
+// const ORIGIN_URL = "192.168.1.127:3001";
+// const ORIGIN_URL_SIGN_IN = `http://${ORIGIN_URL}/members/sign_in`;
+// const ORIGIN_URL_SIGN_OUT = `http://${ORIGIN_URL}/members/sign_out`;
+// const ORIGIN_URL_NEWS = `http://${ORIGIN_URL}/news`;
+// const ORIGIN_URL_PASSWORD_NEWS = `http://${ORIGIN_URL}/members/password/new`;
 // const APP_PARAM = "flag_app=true";
-// const BASE_URL = `https://zwei-test:MsVfM7aVBf@${ORIGIN_URL}`;
+// const BASE_URL = `http://${ORIGIN_URL}`;
 // const PARAM_URL = `${BASE_URL}?${APP_PARAM}`;
+
+const ORIGIN_URL_SIGN_IN = `https://${ORIGIN_URL}/members/sign_in`;
+const ORIGIN_URL_SIGN_OUT = `https://${ORIGIN_URL}/members/sign_out`;
+const ORIGIN_URL_NEWS = `https://${ORIGIN_URL}/news`;
+const ORIGIN_URL_PASSWORD_NEWS = `https://${ORIGIN_URL}/members/password/new`;
+const APP_PARAM = "flag_app=true";
+const BASE_URL = `https://zwei-test:MsVfM7aVBf@${ORIGIN_URL}`;
+const PARAM_URL = `${BASE_URL}?${APP_PARAM}`;
 
 let paymentParams;
 
@@ -65,8 +65,8 @@ const ZweiWebview = () => {
                         : `${_notiUrl}?${APP_PARAM}`;
                     const sliceUrl = _url.slice(8);
                     // TODO: Localhost
-                    // const openUrl = "https://zwei-test:MsVfM7aVBf@" + sliceUrl;
-                    const openUrl = "http://" + sliceUrl;
+                    const openUrl = "https://zwei-test:MsVfM7aVBf@" + sliceUrl;
+                    // const openUrl = "http://" + sliceUrl;
                     setUrl(openUrl);
                     // setInitialRoute(remoteMessage.data.type) // e.g. "Settings"
                 }
@@ -90,7 +90,7 @@ const ZweiWebview = () => {
                 ? `${_notiUrl}&${APP_PARAM}`
                 : `${_notiUrl}?${APP_PARAM}`;
             // TODO: Localhost
-            // setUrl(_url.replace("http://", "https://"));
+            setUrl(_url.replace("http://", "https://"));
             setWebKey(webKey + 1); //reset webview
             resetNotiData();
             return;

12e9707df3cc149dbdce63f477b88e0d14e52aca
HungLM <hunglm@its-global.vn>
2022-07-28 08:39:01 +0700

    Fix recaptcha

--- src/screens/ZweiWebview/index_old.js
+++ src/screens/ZweiWebview/index_old.js
@@ -1,65 +1,53 @@
 /** @format */
 
 // import analytics from '@react-native-firebase/analytics'
-import React, {useCallback, useEffect, useRef, useState} from "react";
-import {ActivityIndicator, Linking, Platform, SafeAreaView, StyleSheet, View,} from "react-native";
+import React, { useState, useCallback, useEffect, useRef } from "react";
+import {
+    SafeAreaView,
+    View,
+    StyleSheet,
+    Platform,
+    ActivityIndicator,
+    Text,
+    Linking,
+} from "react-native";
 
-import {WebView} from "react-native-webview";
-import {useAppState, useNotificationHandler, useShortcutBadge,} from "../../hooks";
+import { WebView } from "react-native-webview";
+import {
+    useNotificationHandler,
+    useShortcutBadge,
+    useAppState,
+} from "../../hooks";
 
 import messaging from "@react-native-firebase/messaging";
 
-// TODO: STG4
-// const ORIGIN_URL = "stg4.zwei-test.com";
-// TODO: DEV
-// const ORIGIN_URL = "dev.zwei-test.com";
-// TODO: STG5-3
 const ORIGIN_URL = "stg5-3.zwei-test.com";
-// TODO: Product
-// const ORIGIN_URL = "app.zwei.ne.jp";
-// TODO: Localhost
-// const ORIGIN_URL = "192.168.1.127:3001";
-// const ORIGIN_URL_SIGN_IN = `members/sign_in`;
-// const ORIGIN_URL_NEWS = `http://${ORIGIN_URL}/news`;
-// const ORIGIN_URL_PASSWORD_NEWS = `http://${ORIGIN_URL}/members/password/new`;
-// const APP_PARAM = "flag_app=true";
-// const IS_MOBILE = "is_mobile=true";
-// const BASE_URL = `http://${ORIGIN_URL}`;
-// const PARAM_URL = `${BASE_URL}?${APP_PARAM}`;
-
-const ORIGIN_URL_SIGN_IN = `members/sign_in`;
+const ORIGIN_URL_SIGN_IN = `https://${ORIGIN_URL}/members/sign_in`;
+const ORIGIN_URL_SIGN_OUT = `https://${ORIGIN_URL}/members/sign_out`;
 const ORIGIN_URL_NEWS = `https://${ORIGIN_URL}/news`;
 const ORIGIN_URL_PASSWORD_NEWS = `https://${ORIGIN_URL}/members/password/new`;
 const APP_PARAM = "flag_app=true";
-const IS_MOBILE = "is_mobile=true";
 const BASE_URL = `https://zwei-test:MsVfM7aVBf@${ORIGIN_URL}`;
+// const PARAM_URL = `${BASE_URL}/members/sign_in${APP_PARAM}`;
 const PARAM_URL = `${BASE_URL}?${APP_PARAM}`;
 
-const ZweiWebview = () => {
+const ZweiWebviewOld = () => {
     const [onAppStateChange] = useAppState();
-    const {notificationHandler, notiData, resetNotiData} =
+    const { notificationHandler, notiData, resetNotiData } =
         useNotificationHandler();
     notificationHandler();
 
-    const {setBadge} = useShortcutBadge();
+    const { setBadge } = useShortcutBadge();
 
     const [deviceToken, setDeviceToken] = useState("");
     const [deviceType, setDeviceType] = useState("");
-    const [webviewUrl, setWebviewUrl] = useState(PARAM_URL);
+    const [url, setUrl] = useState(PARAM_URL);
     const [webKey, setWebKey] = useState(0);
-
-    const js = `
-            try {
-                document.querySelector('.grecaptcha-badge').style.display = 'none';
-                window.document.getElementById('member_device_token').value = '${deviceToken}';
-                window.document.getElementById('member_device_name').value = '${deviceType}';
-            }
-            catch(err) {}
-        `;
 
     const webviewRef = useRef();
 
     useEffect(() => {
+        // Check whether an initial notification is available
         messaging()
             .getInitialNotification()
             .then((remoteMessage) => {
@@ -69,14 +57,13 @@ const ZweiWebview = () => {
                         ? `${_notiUrl}&${APP_PARAM}`
                         : `${_notiUrl}?${APP_PARAM}`;
                     const sliceUrl = _url.slice(8);
-                    // TODO: Localhost
                     const openUrl = "https://zwei-test:MsVfM7aVBf@" + sliceUrl;
-                    // const openUrl = "http://" + sliceUrl;
-                    setWebviewUrl(openUrl);
+                    setUrl(openUrl);
+                    // setInitialRoute(remoteMessage.data.type) // e.g. "Settings"
                 }
+                // setLoading(false)
             });
     }, []);
-
 
     useEffect(() => {
         initNotification();
@@ -92,16 +79,25 @@ const ZweiWebview = () => {
             const _url = _notiUrl.includes("?")
                 ? `${_notiUrl}&${APP_PARAM}`
                 : `${_notiUrl}?${APP_PARAM}`;
-            // TODO: Localhost
-            setWebviewUrl(_url.replace("http://", "https://"));
+            setUrl(_url.replace("http://", "https://"));
             setWebKey(webKey + 1); //reset webview
             resetNotiData();
+            return;
         }
+        // setUrl(PARAM_URL);
     }, [notiData]);
 
     const onNavigationStateChange = (webViewState) => {
-        const {url} = webViewState;
-        console.log("onNavigationStateChange-->", includeUrlParams(url));
+        const { url } = webViewState;
+        if (!url.includes("?flag_app=true")) {
+            if (
+                url === ORIGIN_URL_SIGN_IN ||
+                url === ORIGIN_URL_NEWS ||
+                url === ORIGIN_URL_PASSWORD_NEWS
+            ) {
+                setUrl(url + "?flag_app=true");
+            }
+        }
     };
 
     const onResetNotificationCount = useCallback(
@@ -112,6 +108,7 @@ const ZweiWebview = () => {
                     token || deviceToken
                 }`
             );
+            // .then((res) => console.log("res", res));
         },
         [fetch, deviceToken, setBadge]
     );
@@ -131,91 +128,68 @@ const ZweiWebview = () => {
     const renderLoadingIndicatorView = () => {
         return (
             <View style={styles.loadingWrapper}>
-                <ActivityIndicator color="#009b88" size="large"/>
+                <ActivityIndicator color="#009b88" size="large" />
             </View>
         );
     };
 
-    function includeUrlParams(url) {
-        let _url = url;
-        if (!_url.includes("flag_app=true")) {
-            if (
-                _url === ORIGIN_URL ||
-                _url.includes(ORIGIN_URL_SIGN_IN) ||
-                _url === ORIGIN_URL_NEWS ||
-                _url === ORIGIN_URL_PASSWORD_NEWS
-            ) {
-                _url = _url.includes("?")
-                    ? `${_url}&${APP_PARAM}`
-                    : `${_url}?${APP_PARAM}`;
-            }
-        }
-        // Register confirmation
-        if (_url.includes("register_confirmations") && !_url.includes(IS_MOBILE)) {
-            _url = _url.includes("?")
-                ? `${_url}&${IS_MOBILE}`
-                : `${_url}?${IS_MOBILE}`;
-        }
-        if (_url !== url) {
-            setWebviewUrl(_url);
-        }
-        return _url;
-    }
-
     const renderWebview = () => {
+        const js = `
+      window.document.getElementById('member_device_token').value = '${deviceToken}';
+      window.document.getElementById('member_device_name').value = '${deviceType}';
+      document.querySelector('.grecaptcha-badge').style.display = 'none';
+    `;
+
         return (
             <WebView
                 key={webKey}
                 ref={webviewRef}
                 source={{
-                    uri: webviewUrl,
+                    uri: url,
                 }}
                 style={styles.webview}
                 showsVerticalScrollIndicator={false}
                 javaScriptEnabled={true}
-                javaScriptCanOpenWindowsAutomatically={true}
+                onNavigationStateChange={onNavigationStateChange}
+                javaScriptEnabledAndroid={true}
                 onMessage={(event) => {
                     console.log("event-->", event);
                 }}
                 injectedJavaScript={js}
                 startInLoadingState={true}
                 onShouldStartLoadWithRequest={(event) => {
-                    const {url} = event;
+                    const { url } = event;
+
+                    console.log(url,'abscklbaskcj')
                     if (
                         Platform.OS === "android" &&
                         url &&
                         !url.includes("?flag_app=true")
                     ) {
-                        console.log('Including params: ' + includeUrlParams(url));
-                    } else {
-                        console.log('Loading: ' + url);
-                    }
-                    if (url.includes('token')) {
-                        let _url = url;
-                        if (!url.includes("flag_app=true")) {
-                            _url = url.includes("?")
-                                ? `${url}&${APP_PARAM}`
-                                : `${url}?${APP_PARAM}`;
+                        if (
+                            url === ORIGIN_URL_SIGN_IN ||
+                            url === ORIGIN_URL_NEWS ||
+                            url === ORIGIN_URL_PASSWORD_NEWS
+                        ) {
+                            setUrl(url + "?flag_app=true");
+                        } else {
+                            setUrl(url);
                         }
-                        console.log('Opening link: ' + _url);
-                        Linking.openURL(_url);
-                        return false;
-                    } else if (
+                    }
+                    // if (url && url.includes("?flag_app=true")) {
+                    //   setUrl(url);
+                    // }
+
+                    if (
                         !url ||
                         url.includes(ORIGIN_URL) ||
-                        url.includes("sign_in") ||
                         url.includes("recaptcha.net")
                     ) {
-                        console.log('Handling url: ' + url)
-                        webviewRef.current.injectJavaScript(js);
+                        url.includes("sign_in") && webviewRef.current.injectJavaScript(js);
+
                         return true;
-                    } else if (!url.includes('recaptcha') &&
-                        !url.includes('about:blank')) {
-                        console.log('Opening link: ' + url);
-                        Linking.openURL(url);
-                        return false;
                     }
-                    console.log('Doing nothing with: ' + url);
+                    Linking.openURL(url);
                     return false;
                 }}
                 renderLoading={renderLoadingIndicatorView}
@@ -237,9 +211,6 @@ const styles = StyleSheet.create({
     webview: {
         flex: 1,
     },
-    'grecaptcha-badge': {
-        display: 'none'
-    },
     loadingWrapper: {
         flex: 1,
         position: "absolute",
@@ -252,4 +223,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default React.memo(ZweiWebview);
+export default React.memo(ZweiWebviewOld);

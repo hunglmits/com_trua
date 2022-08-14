a82763c39f08a10ef663bfde97c3db90e9b03395
hunglm <hunglm@its-global.vn>
2022-08-14 16:45:18 +0700

    Update port

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { readConfigFromFile } from "./app/constants/constants.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const port = 3009;
+const port = process.env.PORT || 3009;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
